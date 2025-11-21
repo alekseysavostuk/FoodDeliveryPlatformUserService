da1c109b529761704738536918020e1a894be5f1
Aleksey Savostuk <savostukaleksei@gmail.com>
2025-11-21 23:31:45 +0300

    update unit tests

--- src/test/java/v1/foodDeliveryPlatform/rest/AddressControllerTest.java
+++ src/test/java/v1/foodDeliveryPlatform/rest/AddressControllerTest.java
@@ -1,7 +1,6 @@
 package v1.foodDeliveryPlatform.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/test/java/v1/foodDeliveryPlatform/rest/UserControllerTest.java
+++ src/test/java/v1/foodDeliveryPlatform/rest/UserControllerTest.java
@@ -19,11 +19,12 @@ import v1.foodDeliveryPlatform.security.expression.CustomSecurityExpression;
 import java.util.List;
 import java.util.UUID;
 
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
 @Import(ControllerTestSecurityConfig.class)
